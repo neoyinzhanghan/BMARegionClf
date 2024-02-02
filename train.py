5506b4af99234625081f55ddbb7bf09e91ea6b67
Neo Yin <yinzhang@berkeley.edu>
2024-02-01 20:17:37 -0800

    new scripts

--- train.py
+++ train.py
@@ -52,7 +52,7 @@ class ImageDataModule(pl.LightningDataModule):
 class ResNetModel(pl.LightningModule):
     def __init__(self, num_classes=3):
         super().__init__()
-        self.model = models.resnet18(pretrained=True)
+        self.model = models.resnet50(weights=models.ResNet50_Weights.IMAGENET1K_V1)
         self.model.fc = nn.Linear(self.model.fc.in_features, num_classes)
 
         self.train_accuracy = Accuracy()
