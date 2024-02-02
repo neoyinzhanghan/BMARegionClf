3324c1eb83e569c25bbf2d49037b2715f2606e6b
Neo Yin <yinzhang@berkeley.edu>
2024-02-01 17:29:54 -0800

    new scripts

--- get_magnifications_info.py
+++ get_magnifications_info.py
@@ -1,14 +1,14 @@
-bma_dir = "/media/hdd1/BMAs"
-save_dir = "/media/hdd1"
-
 import os
+import pyvips
+import pandas as pd
+from tqdm import tqdm
 
 # traverse through all the ndpi files in the BMA directory
 # save the magnification of every single levels of each BMA file in a pandas dataframe and save it as a csv file named magnification.csv
 # use the pyvips library to read the ndpi files
-import pyvips
 
-import pandas as pd
+bma_dir = "/media/hdd1/BMAs"
+save_dir = "/media/hdd1"
 
 # first figure out the maximum number of levels in all the BMA files
 max_levels = 0
@@ -30,16 +30,16 @@ for i in range(max_levels):
 # create the dataframe
 df = pd.DataFrame(columns=columns)
 
-# fill the dataframe
-for root, dirs, files in os.walk(bma_dir):
-    for file in files:
-        if file.endswith(".ndpi"):
-            filename = os.path.join(root, file)
-            bma = pyvips.Image.new_from_file(filename, level=0)
-            levels = bma.get("openslide.level-count")
-            magnifications = [bma.get("openslide.level-mag", i) for i in range(levels)]
-            magnifications.insert(0, filename)
-            df = df.append(pd.Series(magnifications, index=df.columns), ignore_index=True)
+# fill the dataframe, use tqdm
+bma_files = [file for file in os.listdir(bma_dir) if file.endswith(".ndpi")]
+
+for file in tqdm(bma_files, total=len(bma_files)):
+    filename = os.path.join(bma_dir, file)
+    bma = pyvips.Image.new_from_file(filename, level=0)
+    levels = bma.get("openslide.level-count")
+    magnifications = [bma.get("openslide.level-mag", i) for i in range(levels)]
+    row = [filename] + magnifications
+    df = df.append(pd.Series(row, index=df.columns), ignore_index=True)
 
 # save the dataframe as a csv file
 df.to_csv(os.path.join(save_dir, "magnification.csv"), index=False)
