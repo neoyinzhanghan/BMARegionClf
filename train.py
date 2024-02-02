2f4128b8943d08d1adda3e977566425040068fab
Neo Yin <yinzhang@berkeley.edu>
2024-02-01 22:02:13 -0800

    new scripts

--- train.py
+++ train.py
@@ -112,7 +112,7 @@ def train_model(downsample_factor):
 
     # Trainer configuration for distributed training
     trainer = pl.Trainer(
-        max_epochs=1, 
+        max_epochs=10, 
         logger=logger, 
         devices=3, 
         accelerator='gpu'  # 'ddp' for DistributedDataParallel
